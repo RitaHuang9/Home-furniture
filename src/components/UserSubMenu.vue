a4f1ee8568953a0e82123e35814bb19becdd31de
ritahuang <ritahuang@hitrust.com.tw>
2023-03-20 09:43:07 +0800

    add:header footer

--- src/components/UserSubMenu.vue
+++ src/components/UserSubMenu.vue
@@ -1,5 +1,37 @@
 <template>
-    <div>
-        這是測選單
+  <div class="subMenu">
+    <div class="subMenu-box">
+      <div class="subMenu-icon">
+        <RouterLink
+          :to="{
+            name: 'login'
+          }"
+          class="subMenu-icon-link subMenu-icon-shopping"
+          ><img src="@/assets/images/shopping-car.svg" alt="" />
+          購物車
+        </RouterLink>
+        <RouterLink
+          :to="{
+            name: 'login'
+          }"
+          class="subMenu-icon-link subMenu-icon-member"
+          ><img src="@/assets/images/member.svg" alt="" /> 登入</RouterLink
+        >
+      </div>
+      <RouterLink class="subMenu-box-link" to="/">最新消息</RouterLink>
+      <RouterLink class="subMenu-box-link"
+        :to="{
+          name: 'login'
+        }"
+        >了解我們</RouterLink
+      >
+      <RouterLink class="subMenu-box-link"
+        :to="{
+          name: 'login'
+        }"
+        >傢俱選購</RouterLink
+      >
+      
     </div>
+  </div>
 </template>
--- src/views/User/Index.vue
+++ src/views/User/Index.vue
@@ -1,5 +1,12 @@
 <template>
-    <div>
-     前台首頁
+  <div class="index-banner">
+    <div class="index-decorate"></div>
+    <div class="index-content">
+      <h2 class="index-banner-title">
+        we create your <br />
+        space better
+      </h2>
+      <h3 class="index-banner-txt">讓你在家的每時每刻都是享受</h3>
     </div>
+  </div>
 </template>
